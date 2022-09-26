e434bd46384a06c7315d3a0bae4b3049aba49264
Coimbra_Tec <faria.d2003@gmail.com>
2022-09-26 18:40:02 +0000

    ex1 feito

--- Ex1.py
+++ Ex1.py
@@ -8,3 +8,18 @@ Após o utilizador ter inserido os 5 valores apresente:
 - O maior valor inserido
 - A média das vendas
 """
+
+if __name__ == '__main__':  # isto é o ponto de partida
+    print(f'Insira 5 dados')
+    listaVendas = [int(input()) for c in range(5)]#100, 50, 200, 300, 400 f'Insira {ndados -1} valores inteiros'
+    soma = listaVendas.__len__()
+    listailhas = ['Terceira', 'Pico', 'Faial', 'São Jorge', 'Graciosa']
+    media = sum(listaVendas) / soma
+    soma = sum(listaVendas)
+    minimo = min(listaVendas)
+    maximo = max(listaVendas)
+
+    print(f'Mínimo Valor {minimo}')
+    print(f'Máximo Valor {maximo}')
+    print(f'Total de Vendas {soma}')
+    print(f'Média {media}')
