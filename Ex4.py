4ece260f74c799960e8eeafca59412cb99d9d1aa
ciberseguranca <diogobrunofaria@enta.pt>
2022-09-28 08:41:10 +0000

    exe 4 e 5 feitos

--- Ex4.py
+++ Ex4.py
@@ -1,26 +1,53 @@
+
+
 """
-Declare uma lista para guardar as vendas do grupo central feito
-Declara uma lista para guardar os nomes das cinco ilhas do grupo central feito
-Peça dados ao utilizador e guarde-os na lista feito
-Após o utilizador ter inserido os 5 valores apresente: feito
-- Total das vendas feito
+Declare uma lista para guardar as vendas do grupo central
+Declara uma lista para guardar os nomes das cinco ilhas do grupo central
+Peça dados ao utilizador e guarde-os na lista
+Após o utilizador ter inserido os 5 valores apresente:
+- Total das vendas
 - O menor valor inserido assim como as respetivas ilhas
 - O maior valor inserido assim como as respetivas ilhas
-- A média das vendas feito
+- A média das vendas
 """
-if __name__ == '__main__':
-    print(f'Incira 5 valores')
-    ListadeVendas = [int(input()) for c in range(5)]
-    Tamanho = ListadeVendas.__len__()
-    ListadeNomes = ['Terceira', 'Pico', 'Faial', 'São Jorge', 'Graciosa']
 
-    soma = sum(ListadeVendas)
-    minimo = min(ListadeVendas)
-    maximo = max(ListadeVendas)
-    media = soma / Tamanho
-    print(f'Total da soma é {soma}')
-    print(f'A venda mais alta é {maximo} e vem da ilha {ListadeNomes}')
-    print(f'A venda mais baixa foi {minimo} e vem da ilha {ListadeNomes}')
-    print(f'A media é de {media}')
-    print(f'A lista de vendas é {ListadeVendas}')
+#print(chr(ord('A') + 32))
+
+if __name__ == '__main__':
+    user_info = []
+    vendas = []
+    ilhas = ['Terceira', 'Graciosa', 'Pico', 'Faial', 'São Jorge']
+
+    user_info.append(input('Qual seu nome? '))
+    user_info.append(input('Qual sua idade? '))
+    user_info.append(input('Qual sua morada? '))
+    user_info.append(input('É natural de  '))
+
+    for ilha in ilhas:
+        vendas.append(int(input(f'Qual é o número de vendas para {ilha}? ')))
+
+    total_vendas = 0
+    for x in vendas:
+        total_vendas += x
+
+    media_vendas = total_vendas / len(vendas)
+
+    print(f'O total de vendas é {total_vendas}')
+    print(f'A média de vendas é {media_vendas}')
+
+    menor_vendas = vendas[0]
+    maior_vendas = vendas[0]
+    for x in range(1, len(vendas)):
+        if vendas[x] < menor_vendas:
+            menor_vendas = vendas[x]
+        if vendas[x] > maior_vendas:
+            maior_vendas = vendas[x]
+
+    comp = 0
+    for x in vendas:
+        if x == menor_vendas:
+            print(f'O menor valor de vendas foi {menor_vendas} de {ilhas[comp]}')
+        if x == maior_vendas:
+            print(f'O maior valor de vendas foi {maior_vendas} de {ilhas[comp]}')
+        comp += 1
 
