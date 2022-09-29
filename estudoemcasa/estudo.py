1546a5131598dc1d7a0ece5040278c5c7c4a9f6b
Coimbra_Tec <faria.d2003@gmail.com>
2022-09-29 20:28:14 +0000

    estudo feito

---
+++ estudoemcasa/estudo.py
@@ -0,0 +1,30 @@
+"""
+somar os valores de uma lista
+
+"""
+
+import random
+
+
+def gerador(ini, fim):
+    return random.randrange(ini, fim + 1)
+
+
+if __name__ == '__main__':
+
+    nums = [0, 0, 0, 0, 0]
+    bonus = [0, 0]
+    for x in range(len(nums)):
+        while True:
+            onumero = gerador(1, 50)
+            if onumero not in nums:
+                nums[x] = onumero
+                break
+        print(f'Numeros = {nums}')
+        for x in range(len(bonus)):
+            while True:
+                onumero = gerador(1, 12)
+                if onumero not in bonus:
+                    bonus[x] = onumero
+                    break
+        print(f'bonus = {bonus}')
