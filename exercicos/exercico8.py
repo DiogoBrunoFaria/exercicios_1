fe41fb626a87b342061be763f2d880bf329b9db1
ciberseguranca <diogobrunofaria@enta.pt>
2022-09-29 12:01:47 +0000

    exercicio 8 v2 jogos santa casa

--- exercicos/exercico8.py
+++ exercicos/exercico8.py
@@ -1,6 +1,7 @@
 """
 lista com 5 casas
 lista que gera numeros entre 1-50
+maquina de numeros aleatorios
 """
 import random
 
@@ -11,11 +12,18 @@ def gerador(ini, fim):
 
 if __name__ == '__main__':
     nums = [0, 0, 0, 0, 0]
+    bonus = [0, 0]
     for x in range(len(nums)):
         while True:
-            onumero = gerador(1, 5)
+            onumero = gerador(1, 50)
             if onumero not in nums:
                 nums[x] = onumero
                 break
-    print(nums)
-
+    print(f'Numeros = {nums}')
+    for x in range(len(bonus)):
+        while True:
+            onumero = gerador(1, 12)
+            if onumero not in bonus:
+                bonus[x] = onumero
+                break
+    print(f'bonus = {bonus}')
