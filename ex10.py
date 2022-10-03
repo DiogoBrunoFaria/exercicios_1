6c7ecff927fa36b4096643e8452cf5a39d48bcf8
ciberseguranca <diogobrunofaria@enta.pt>
2022-10-03 10:21:12 +0000

    inicio do exercicio 9

--- ex10.py
+++
@@ -1,4 +0,0 @@
-"""
-search and replace
-escrever uma fazer e conguir editar uma palavra
-"""
--- ex9.py
+++ ex9.py
@@ -3,15 +3,25 @@ primeiro ex
 """
 
 if __name__ == '__main__':
+    """
     x = int(input(f'Escreva o primeiro numero '))
     y = int(input(f'Escreva o segundo numero '))
     z = (x + 3 * y) * (x - y)
     print(f'O valor de {x + 3 * y} * {x - y} e: {z}')
-
+    """
 """
 segundo ex
 """
-tamanho = input(f'Quantos numeros quer incerir?')
-listaKM = [int(input(tamanho)) for c in range(5)]
-listaM = []
+
+listaKM = [int(input()) for c in range(5)]
+comp = listaKM.__len__()
+mediaKM = sum(listaKM) / comp
+print(f'A media em KM é de {mediaKM}')
+
+listaM = [int(input()) for c in range(5)]
+comp = listaKM.__len__()
+mediaM = sum(listaKM) / comp
+print(f'A media em M é de {mediaM}')
+
 print(listaKM)
+print(comp)
