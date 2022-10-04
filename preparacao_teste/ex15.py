1b0fe62dd37f0be72d2985e77b0098dce12dd8a4
ciberseguranca <diogobrunofaria@enta.pt>
2022-10-04 11:36:10 +0000

    exercicio 15 feito

--- preparacao_teste/ex15.py
+++ preparacao_teste/ex15.py
@@ -4,4 +4,23 @@ calcula o inteiro que tem esses dígitos. Por exemplo, lendo os dígitos 1 5
 4 5 8 -1, calcula o número inteiro 15458.
 """
 
-if __name__ == ' __main__':
+if __name__ == '__main__':
+    num_list = []
+    while True:
+        try:
+            num = int(input('Digite um número: '))
+
+            if num < 0:
+                break
+
+            num_list.append(num)
+
+        except ValueError:
+            print('Digite um valor válido')
+
+    num = ''
+    for x in range(len(num_list)):
+        num += str(num_list[x])
+
+    print(f'O número é: {int(num)}')
+
